1570bb085fbe95e028e887827515f4f403b938c3
Yutian Guo <yutian@mail.com>
2025-03-13 23:39:18 +0100

    1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavBarComponent } from "./componentes/nav-bar/nav-bar.component";
+import { PrincipalComponent } from "./componentes/principal/principal.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavBarComponent, PrincipalComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/componentes/principal/principal.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-principal',
+  imports: [],
+  templateUrl: './principal.component.html',
+  styleUrl: './principal.component.css'
+})
+export class PrincipalComponent {
+
+}
