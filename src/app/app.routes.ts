f4391e64dac34d6e1f4decd4b33a5679e7d01f71
Yutian Guo <yutian@mail.com>
2025-03-26 23:54:01 +0100

    actualizacionnew

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { HomeComponent } from './componentes/home/home.component';
 import { AlbumComponent } from './componentes/album/album.component';
 import { PreVentasYexclusivosComponent } from './componentes/pre-ventas-y-exclusivos/pre-ventas-y-exclusivos.component';
 import { NevelandZoneComponent } from './componentes/neveland-zone/neveland-zone.component';
+import { MerchOficialComponent } from './componentes/merch-oficial/merch-oficial.component';
+import { EnviosYSoporteComponent } from './componentes/envios-y-soporte/envios-y-soporte.component';
 
 
 export const routes: Routes = [
@@ -11,5 +13,6 @@ export const routes: Routes = [
     {path: 'albumes', component: AlbumComponent},
     {path: 'preventas y exclusivas', component: PreVentasYexclusivosComponent},
     {path: 'neverland', component: NevelandZoneComponent},
-    {path: ''}
+    {path: 'merchandising', component: MerchOficialComponent},
+    {path: 'envios-y-soporte', component: EnviosYSoporteComponent}
 ];
